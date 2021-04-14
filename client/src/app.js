da5ff238efb588430c572a559985de85bc6d89a7
QuinntonCarter <qdcarter92@protonmail.com>
2021-04-14 14:01:37 -0600

    Adding recipeCard and forms

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RecipeCard from './components/RecipeCard.js'
+import Login from './forms/Login.js'
+import CreateAccount from './forms/CreateAccount.js'
+
+export default function App(){
+    return(
+        <>
+        
+        </>
+    )
+}
---
+++ client/src/forms/CreateAccount.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function CreateAccount(){
+    return(
+        <div className='createAccountContainer'>
+            <i className="fas fa-user-circle" style={{fontSize: '260px', color: 'rgba(200, 16, 46, 1)'}}></i>
+            <h2> Create New Account </h2>
+            <form>
+                First Name
+                <input placeholder='First Name'></input>
+                Last Name
+                <input placeholder='Last Name'></input>
+                E-Mail
+                <input placeholder='E-Mail'></input>
+                Password
+                <input placeholder='Password'></input>
+            </form>
+                <input type='checkbox'/> I agree to terms of service and privacy policy
+                <button className='signUpBtn'> Sign Up </button>
+        </div>
+    )
+}
