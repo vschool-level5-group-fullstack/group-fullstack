a670da7a92edfcef57110560b2e715689db8a280
Amber Worthy <ambercodes3@gmail.com>
2021-05-06 10:35:46 -0700

    Edit

---
+++ client/src/components/Edit.js
@@ -0,0 +1,19 @@
+import React, {useContext, useState} from "react"
+
+
+function Edit() {
+const 
+
+
+
+
+    return(
+        <div>
+            <h2>Ingredients</h2>
+            <h2>Instructions</h2>
+        </div>
+
+    )
+}
+
+export default Edit 
