32487e847233ec90a7ff9a07427cdc48a3c25951
QuinntonCarter <qdcarter92@protonmail.com>
2021-05-11 11:06:05 -0600

    made the dash smarter

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,36 @@
-import React, {useContext} from 'react'
-import RecipeCard from './RecipeCard.js'
-import {AppContext} from '../appContext'
+import React, { useEffect, useState, useContext} from 'react';
+import RecipeCard from './RecipeCard.js';
+import {AppContext} from '../appContext';
+
+import date from 'date-and-time';
 
 
 const Dashboard = () => {
     const {currentUser} = useContext(AppContext)
-    const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    // const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    const [day, setDay] = useState('')
+
+    const now = new Date();
+    const thisWeek = []
+
     const btnText = 'EDIT'
 
-    const recipeCardComponent = week.map((day) => {
+    function getWeekDays(num){
+        const next = date.addDays(now, +num)
+        return date.format(next, 'dddd')
+    }
+    
+    for(let i = 0; i <= 6; i++){
+        const weeklyDays = getWeekDays(i)
+        thisWeek.push(weeklyDays)
+    }
+
+    function findDay(){
+        return setDay(date.format(now, 'dddd'))
+    }   
+
+    const recipeCardComponent = thisWeek.map(day => {
         return(
             <div className="dashBoardCard">
                 <h3> {day} </h3>
@@ -17,7 +39,9 @@ const Dashboard = () => {
         )
     })
 
-
+    useEffect(() => {
+        findDay()
+    },[])
 
     return (
         <div className="dashBoard">
